46e009862af6d4844f0e2fe88875195c98c7fa84
Gurpreet Singh <gurpreet.badhan.28@gmail.com>
2024-09-17 08:23:29 +0530

    basic project

---
+++ src/main/java/com/springboot_grpc/SpringbootgrpcApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootgrpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootgrpcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot_grpc/SpringbootgrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot_grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootgrpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
